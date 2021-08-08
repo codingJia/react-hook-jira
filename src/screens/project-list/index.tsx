06177fc8a9791e6dced7686524188b4231cbaa3d
codingJia <786723920@qq.com>
2021-08-08 18:45:38 +0800

    chore：添加ts类型检测

---
+++ src/screens/project-list/index.tsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import { List } from "./list";
+import { SearchPanel } from "./search-panel";
+import { cleanObject, useDebounce, useMount } from "../../utils";
+import qs from "qs";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const ProjectListScreen = () => {
+  const [users, setUsers] = useState([]);
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const debouncedParam = useDebounce(param, 2000);
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
+    ).then(async (res) => {
+      if (res.ok) {
+        setList(await res.json());
+      }
+    });
+  }, [debouncedParam]);
+  useMount(() => {
+    fetch(`${apiUrl}/users`).then(async (res) => {
+      if (res.ok) {
+        setUsers(await res.json());
+      }
+    });
+  });
+  return (
+    <div>
+      <SearchPanel param={param} setParam={setParam} users={users} />
+      <List list={list} users={users} />
+    </div>
+  );
+};
